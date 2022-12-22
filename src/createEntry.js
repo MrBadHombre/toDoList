63512fd81de6314bc30b9a4161f396b47d92c7b6
Michael Margrave <l1ghtdev.88@gmail.com>
2022-12-21 17:28:20 -0800

    create grid layout of page and add containers for todo's

--- src/createEntry.js
+++ src/createEntry.js
@@ -1,12 +1,25 @@
-const createAllElements = function (title, content, priority) {
-  const newEntry = (title, content, priority) => {
-    return { title, content, priority };
+const createAllElements = function (
+  title,
+  content,
+  priority,
+  timeToComplete,
+  completionDate
+) {
+  const newEntry = (
+    title,
+    content,
+    priority,
+    timeToComplete,
+    completionDate
+  ) => {
+    return { title, content, priority, timeToComplete, completionDate };
   };
 
   const createDivElement = (entry) => {
     const contents = document.getElementById('content');
     const divdiv = document.createElement('div');
-    divdiv.innerHTML = `${entry.title}`;
+    divdiv.classList.add('divdiv');
+    divdiv.innerHTML = `<div>${entry.title}</div><div>${entry.content}</div><div>${entry.priority}</div><div>${entry.timeToComplete}</div><div>${entry.completionDate}`;
     contents.appendChild(divdiv);
   };
 
@@ -14,6 +27,8 @@ const createAllElements = function (title, content, priority) {
     title,
     content,
     priority,
+    timeToComplete,
+    completionDate,
     newEntry,
     createDivElement,
   };
--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
 import './style.css';
 import { createAllElements } from './createEntry';
-
+const todayItem = document.querySelector('.todayItem');
 const btn = document.querySelector('#addTask');
 const header = document.querySelector('.header');
 const toDo = document.querySelector('#title');
 const description = document.querySelector('#description');
+const divdiv = document.getElementById('divdiv');
 let entry;
 let priority;
 let arraaaay = [];
-// entryList(newEntry(title, description, priority));
-// createToDoList(entryArray);
 
 btn.addEventListener('click', () => {
-  entry = createAllElements(toDo.value, description.value, priority);
+  entry = entry;
+  entry = createAllElements(toDo.value, description.value);
   arraaaay.push(entry);
   console.table(arraaaay);
-  for (let i = 0; i < arraaaay.length; i++) {
-    entry.createDivElement(entry);
-  }
+  entry.createDivElement(entry);
+  todayItem.innerHTML = `${toDo.value}`;
 });
